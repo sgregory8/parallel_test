cb0040c90993d29f506a6bd2c752b4e1f1389b4c
Gregory, Sam (ELS-OXF) <s.gregory@elsevier.com>
2020-07-17 12:03:43 +0100

    added other stuff for parallel

---
+++ src/test/java/com/gregory/learning/Parallel1Test.java
@@ -0,0 +1,20 @@
+package com.gregory.learning;
+
+import org.junit.Test;
+
+public class Parallel1Test {
+
+  @Test
+  public void waitPlease() {
+    try {
+      System.out.println("Waiting from: " + this.getClass().getName());
+      System.out.println("__________----------THREAD_INFO----------__________");
+      System.out.println(Thread.currentThread().getName());
+      System.out.println(Thread.currentThread().getId());
+      Thread.sleep(10000);
+      System.out.println("DONE");
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
